5fcec902fa3a7de180aef33a88f0830f940e0156
chandusoni1 <chandreshsoni8055@gmail.com>
2025-10-19 15:19:05 +0530

    add pages and compo

--- BoomFrontend/src/components/Header.jsx
+++ BoomFrontend/src/components/Header.jsx
@@ -8,29 +8,29 @@ function Header() {
   const handelcreate = () => {
     setmessage("creating a meet....");
   };
-  const [sidebaropen,setsidebaropen]=useState(true)
-  let issidebaropen=true
+  
   return (
     <div className="relative w-screen h-screen   ">
-
       {/* sidebar */}
-
-      <div className=" absolute h-full w-[350px] bg-slate-400">
+        
+      <div className=" absolute h-full w-[350px] bg-slate-400" >
         <span className="flex flex-col gap-1 pl-5 text-black text-4xl">
-        <ul>home</ul>
-        <ul> service</ul>
-        <ul>cart</ul>
-        <ul>iojdi</ul>
+          <ul>home</ul>
+          <ul> service</ul>
+          <ul>cart</ul>
+          <ul>iojdi</ul>
         </span>
       </div>
-        {/* button */}
+      {/* button */}
+      
 
-
-      <button className="absolute flex flex-col h-[50px] w-[50px]   gap-1 pl-2.5 rounded-full justify-center left-100 top-4 ">
+      <button  className="absolute flex flex-col h-[50px] w-[50px]   gap-1 pl-2.5 rounded-full justify-center left-100 top-4 ">
         <div className="bg-white h-[3.5px] w-[25px]"></div>
         <div className="bg-white h-[3.5px] w-[25px]"></div>
         <div className="bg-white h-[3.5px] w-[25px]"></div>
       </button>
+      <button className="absolute left-100 top-18">schedule meet</button>
+
       {/* main contet */}
       <div className=" h-full w-full flex justify-center flex-col items-center text-black  ">
         <h1>BOOM</h1>
---
+++ BoomFrontend/src/pages/ScheduleMeet.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ScheduleMeet() {
+  return (
+    <div>
+      <h1>generate meet /link</h1>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default ScheduleMeet
